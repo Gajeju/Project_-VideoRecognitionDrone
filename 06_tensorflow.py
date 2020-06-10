7ab63df34db44725f00b1315c463bf9aa4501820
Gajeju <45118610+Gajeju@users.noreply.github.com>
2020-06-11 08:46:01 +0900

    tensorflow 2.0 버전에 맞게 일부 수정

--- 06_tensorflow.py
+++ 06_tensorflow.py
@@ -71,8 +71,8 @@ for file in tar_file.getmembers():
 
 detection_graph = tf.Graph()
 with detection_graph.as_default():
-  od_graph_def = tf.GraphDef()
-  with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+  od_graph_def = tf.compat.v1.GraphDef()
+  with tf.io.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
     serialized_graph = fid.read()
     od_graph_def.ParseFromString(serialized_graph)
     tf.import_graph_def(od_graph_def, name='')
